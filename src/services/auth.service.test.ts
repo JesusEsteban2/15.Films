d6e09ef88e822107cbf685e571ca7e551df3e0f9
jesusesteban2 <jesus.esteban@outlook.com>
2025-04-16 13:14:04 +0200

    Update

--- src/services/auth.service.test.ts
+++ src/services/auth.service.test.ts
@@ -1,18 +1,51 @@
-import { hash } from 'bcryptjs';
-import { AuthService } from './auth.service';
+import { hash, compare } from 'bcryptjs';
+import { AuthService, Payload } from './auth.service';
+import jwt from 'jsonwebtoken';
+import { sign } from 'crypto';
 
 vi.mock('bcryptjs', () => ({
     hash: vi.fn().mockResolvedValue('soy un has'),
+    compare: vi.fn().mockResolvedValue(true),
 }));
 
+vi.mock('jsonwebtoken');
+
 describe('Given the class AuthService', () => {
+    const token = 'Este es un token';
+
+    const payload = {
+        id: 'id',
+        email: 'email',
+        role: 'role',
+    } as Payload;
+
     describe('When hashPassword is called', () => {
         test('Then is should call hash form bcript', async () => {
             const password = 'Esto es un password';
             const result = await AuthService.hashPassword(password);
-            expect(hash).toHaveBeenCalledWith(password);
+
+            expect(hash).toHaveBeenCalledWith(password, 10);
             expect(result).toBe('soy un has');
-            console.log(result);
+        });
+    });
+
+    describe('When compare password is called', () => {
+        test('Then it should compare password', async () => {
+            const password = 'Soy una password';
+            const hash = 'Soy un Hash';
+            const result = await AuthService.comparePassword(password, hash);
+
+            expect(result).toBe(true);
+            expect(compare).toHaveBeenCalledWith(password, hash);
+        });
+    });
+
+    describe('When generateToken is called', () => {
+        test('Then call sing with & secret', async () => {
+            //const secret = 'contraseña secreta';
+            const token = await AuthService.generateToken(payload);
+            console.log(token);
+            expect(jwt.sign).toHaveBeenCalledWith(payload);
         });
     });
 });
